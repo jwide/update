95245e083e4e98686f0947111e53a7c9a7737adf
wjwidener <20847594+wjwidener@users.noreply.github.com>
2020-08-29 15:14:53 -0500

    Create updateips2.sh

---
+++ bootstrap/updateips2.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+#
+# Output usage messsage.
+#
+
+if [ "$#" -ne 10 ]; then
+    echo " "
+    echo " Illegal number of parameters"
+    echo " "
+    echo " "
+    echo " Usage: updateips.sh wanip wansubnet-cidr lanip lansubnet-cidr langw-ip wangw-ip network-addr-range network-cidr"
+    echo " "
+    echo " For Example: "
+    echo " "
+    echo " If the IPs, subnet cidr and gw ips are:"
+    echo " MGMT SUBNET RANGE: 192.168.0.64"
+    echo " MGMT SUBNET CIDR: /26"
+    echo " WANIP2 Alias and Virtual IP: 192.168.0.9"
+    echo " WANIP3 Alias and Virtual IP: 192.168.0.11"
+    echo " OVPN TUNNEL NETWORK: 172.31.168.0"
+    echo " OVPN TUNNEL NETWORK CIDR: 24"
+    echo " OVPN LOCAL NETWORK: 192.168.0.0"
+    echo " OVPN LOCAL NETWORK CIDR: 24"
+    echo " "
+    echo " Then the script would be run as: "
+    echo " "
+    echo " /bin/sh updateips2.sh 192.168.0.64 26 192.168.0.9 192.168.0.11 172.31.168.0 24 192.168.0.0 24"
+    echo " "
+    exit 1
+else
+
+sed -i '' -E -e 's/10.10.10.10/'$1'/g' config.xml
+sed -i '' -E -e 's/11.11.11.11/'$2'/g' config.xml
+sed -i '' -E -e 's/12.12.12.12/'$3'/g' config.xml
+sed -i '' -E -e 's/13.13.13.13/'$4'/g' config.xml
+sed -i '' -E -e 's/14.14.14.14/'$5'/g' config.xml
+sed -i '' -E -e 's/15.15.15.15/'$6'/g' config.xml
+sed -i '' -E -e 's/16.16.16.16/'$7'/g' config.xml
+sed -i '' -E -e 's/17.17.17.17/'$8'/g' config.xml
+
+fi
