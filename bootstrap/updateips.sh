b5bdf6c7994eabe3dd9cf37dc4ab074128a5d05a
wjwidener <20847594+wjwidener@users.noreply.github.com>
2020-08-29 04:36:30 -0500

    Update updateips.sh

--- bootstrap/updateips.sh
+++ bootstrap/updateips.sh
@@ -24,10 +24,16 @@ if [ "$#" -ne 10 ]; then
     echo " NETWORK CIDR: /24"
     echo " MGMT SUBNET RANGE: 192.168.0.64"
     echo " MGMT SUBNET CIDR: /26"
+	echo " WANIP2 Alias and Virtual IP: 192.168.0.9"
+	echo " WANIP3 Alias and Virtual IP: 192.168.0.11"
+	echo " OVPN TUNNEL NETWORK: 172.31.168.0"
+	echo " OVPN TUNNEL NETWORK CIDR: 24"
+	echo " OVPN LOCAL NETWORK: 192.168.0.0"
+	echo " OVPN LOCAL NETWORK CIDR: 24"
     echo " "
     echo " Then the script would be run as: "
     echo " "
-    echo " /bin/sh updateips.sh 192.168.0.7 22 192.168.4.7 22 192.168.4.1 192.168.0.1 192.168.0.0 24 192.168.0.64 26"
+    echo " /bin/sh updateips.sh 192.168.0.7 22 192.168.4.7 22 192.168.4.1 192.168.0.1 192.168.0.0 24 192.168.0.64 26 192.168.0.9 192.168.0.11 172.31.168.0 24 192.168.0.0 24"
     echo " "
     exit 1
 else
@@ -42,5 +48,11 @@ sed -i '' -E -e 's/7.7.7.7/'$7'/g' config.xml
 sed -i '' -E -e 's/9.9.9.9/'$8'/g' config.xml
 sed -i '' -E -e 's/10.10.10.10/'$9'/g' config.xml
 sed -i '' -E -e 's/11.11.11.11/'$10'/g' config.xml
+sed -i '' -E -e 's/12.12.12.12/'$11'/g' config.xml
+sed -i '' -E -e 's/13.13.13.13/'$12'/g' config.xml
+sed -i '' -E -e 's/14.14.14.14/'$13'/g' config.xml
+sed -i '' -E -e 's/15.15.15.15/'$14'/g' config.xml
+sed -i '' -E -e 's/16.16.16.16/'$15'/g' config.xml
+sed -i '' -E -e 's/17.17.17.17/'$16'/g' config.xml
 
 fi
