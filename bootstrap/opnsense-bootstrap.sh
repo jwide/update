2d6746b4c1f8f97daa1062411da6d7e45dc68851
James Widener <46571932+jwide@users.noreply.github.com>
2024-06-04 02:51:21 -0500

    Update opnsense-bootstrap.sh
    Fixed for 24.1

--- bootstrap/opnsense-bootstrap.sh
+++ bootstrap/opnsense-bootstrap.sh
@@ -31,7 +31,7 @@ URL="https://github.com/opnsense/core/archive"
 WORKDIR="/tmp/opnsense-bootstrap"
 FLAVOUR="OpenSSL"
 TYPE="opnsense"
-RELEASE="20.7"
+RELEASE="24.1"
 
 DO_ABI="-a ${RELEASE}"
 DO_BARE=
@@ -40,65 +40,65 @@ DO_INSECURE=
 DO_YES=
 
 while getopts a:bfin:r:t:vy OPT; do
-	case ${OPT} in
-	a)
-		DO_ABI="-a ${OPTARG}"
-		;;
-	b)
-		DO_BARE="-b"
-		;;
-	f)
-		DO_FACTORY="-f"
-		;;
-	i)
-		DO_INSECURE="-i"
-		;;
-	n)
-		FLAVOUR=${OPTARG}
-		;;
-	r)
-		RELEASE=${OPTARG}
-		;;
-	t)
-		TYPE=${OPTARG}
-		;;
-	v)
-		echo ${RELEASE}
-		exit 0
-		;;
-	y)
-		DO_YES="-y"
-		;;
-	*)
-		echo "Usage: man ${0##*/}" >&2
-		exit 1
-		;;
-	esac
+        case ${OPT} in
+        a)
+                DO_ABI="-a ${OPTARG}"
+                ;;
+        b)
+                DO_BARE="-b"
+                ;;
+        f)
+                DO_FACTORY="-f"
+                ;;
+        i)
+                DO_INSECURE="-i"
+                ;;
+        n)
+                FLAVOUR=${OPTARG}
+                ;;
+        r)
+                RELEASE=${OPTARG}
+                ;;
+        t)
+                TYPE=${OPTARG}
+                ;;
+        v)
+                echo ${RELEASE}
+                exit 0
+                ;;
+        y)
+                DO_YES="-y"
+                ;;
+        *)
+                echo "Usage: man ${0##*/}" >&2
+                exit 1
+                ;;
+        esac
 done
 
 shift $((${OPTIND} - 1))
 
 if [ "$(id -u)" != "0" ]; then
-	echo "Must be root." >&2
-	exit 1
+        echo "Must be root." >&2
+        exit 1
 fi
 
 FBSDNAME=$(uname -s)
 if [ "${FBSDNAME}" != "FreeBSD" ]; then
-	echo "Must be FreeBSD." >&2
-	exit 1
+        echo "Must be FreeBSD." >&2
+        exit 1
 fi
 
 FBSDARCH=$(uname -p)
 if [ "${FBSDARCH}" != "amd64" ]; then
-	echo "Must be amd64 architecture." >&2
-	exit 1
+        echo "Must be amd64 architecture." >&2
+        exit 1
 fi
 
 FBSDVER=$(uname -r | colrm 4)
 if [ "${FBSDVER}" != "13." ]; then
-	echo "Must be a FreeBSD 13 release." >&2
-	exit 1
+        echo "Must be a FreeBSD 13 release." >&2
+        exit 1
 fi
 
 echo "This utility will attempt to turn this installation into the latest"
@@ -107,49 +107,49 @@ echo "system and kernel will be replaced, and if all went well the system"
 echo "will automatically reboot."
 
 if [ -z "${DO_YES}" ]; then
-	echo
-	echo -n "Proceed with this action? [y/N]: "
+        echo
+        echo -n "Proceed with this action? [y/N]: "
 
-	read YN
-	case ${YN} in
-	[yY])
-		;;
-	*)
-		exit 0
-		;;
-	esac
+        read YN
+        case ${YN} in
+        [yY])
+                ;;
+        *)
+                exit 0
+                ;;
+        esac
 fi
 
 if [ -n "${DO_FACTORY}" ]; then
-	if [ -z "${DO_YES}" ]; then
-		echo
-		echo -n "Factory reset mode selected, are you sure? [y/N]: "
+        if [ -z "${DO_YES}" ]; then
+                echo
+                echo -n "Factory reset mode selected, are you sure? [y/N]: "
 
-		read YN
-		case ${YN} in
-		[yY])
-			;;
-		*)
-			exit 0
-			;;
-		esac
-	fi
+                read YN
+                case ${YN} in
+                [yY])
+                        ;;
+                *)
+                        exit 0
+                        ;;
+                esac
+        fi
 fi
 
 if [ -n "${DO_INSECURE}" ]; then
-	if [ -z "${DO_YES}" ]; then
-		echo
-		echo -n "Insecure mode selected, are you sure? [y/N]: "
+        if [ -z "${DO_YES}" ]; then
+                echo
+                echo -n "Insecure mode selected, are you sure? [y/N]: "
 
-		read YN
-		case ${YN} in
-		[yY])
-			;;
-		*)
-			exit 0
-			;;
-		esac
-	fi
+                read YN
+                case ${YN} in
+                [yY])
+                        ;;
+                *)
+                        exit 0
+                        ;;
+                esac
+        fi
 fi
 
 echo
@@ -162,50 +162,50 @@ mkdir -p ${WORKDIR}
 export ASSUME_ALWAYS_YES=yes
 
 if [ -n "${DO_INSECURE}" ]; then
-	# no CA file around to verify against, user choice
-	export SSL_NO_VERIFY_PEER=yes
+        # no CA file around to verify against, user choice
+        export SSL_NO_VERIFY_PEER=yes
 else
-	pkg bootstrap -f
-	pkg install ca_root_nss
+        pkg bootstrap -f
+        pkg install ca_root_nss
 
-	# save a copy of the CA file to use for the bootstrap
-	cp /etc/ssl/cert.pem ${WORKDIR}/cert.pem
-	export SSL_CA_CERT_FILE=${WORKDIR}/cert.pem
+        # save a copy of the CA file to use for the bootstrap
+        cp /etc/ssl/cert.pem ${WORKDIR}/cert.pem
+        export SSL_CA_CERT_FILE=${WORKDIR}/cert.pem
 fi
 
 SUBFILE="stable/${RELEASE}"
 SUBDIR="stable-${RELEASE}"
 if [ "${RELEASE}" = "snapshot" ]; then
-	SUBFILE="master"
-	SUBDIR="master"
+        SUBFILE="master"
+        SUBDIR="master"
 fi
 
 fetch -o ${WORKDIR}/core.tar.gz "${URL}/${SUBFILE}.tar.gz"
 tar -C ${WORKDIR} -xf ${WORKDIR}/core.tar.gz
 
 if [ -z "${DO_BARE}" ]; then
-	if pkg -N; then
-		pkg unlock -a
-		pkg delete -fa
-	fi
-	rm -f /var/db/pkg/*
+        if pkg -N; then
+                #pkg unlock -a
+                pkg delete -fa
+        fi
+        rm -rf /var/db/pkg/*
 fi
 
 make -C ${WORKDIR}/core-${SUBDIR} bootstrap DESTDIR= \
     CORE_ABI=${DO_ABI#"-a "} CORE_FLAVOUR=${FLAVOUR}
 
 if [ -z "${DO_BARE}" ]; then
-	if [ -n "${DO_FACTORY}" ]; then
-		rm -rf /conf/*
-	fi
+        if [ -n "${DO_FACTORY}" ]; then
+                rm -rf /conf/*
+        fi
 
-	pkg bootstrap
-	pkg install ${TYPE}
+        pkg bootstrap
+        pkg install ${TYPE}
 
-	# beyond this point verify everything
-	unset SSL_NO_VERIFY_PEER
-	unset SSL_CA_CERT_FILE
+        # beyond this point verify everything
+        unset SSL_NO_VERIFY_PEER
+        unset SSL_CA_CERT_FILE
 
-	opnsense-update -bkf
-#	reboot
+        opnsense-update -bkf
+#       reboot
 fi
